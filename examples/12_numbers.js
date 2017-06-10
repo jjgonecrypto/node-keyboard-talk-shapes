a23ccaa703e6d2601543f48f4ad44d023cd63bc2
justin j. moses <justinjmoses@gmail.com>
2017-06-10 17:00:32 -0400

    Updating numbers

--- examples/12_numbers.js
+++ examples/12_numbers.js
@@ -5,7 +5,7 @@ const Rx = require('node-keyboard-rx')()
 // the first 96 digits of PI
 const PI = '3.141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117'
 
-module.exports = (key = 'c', scaleName = 'major') => {
+module.exports = (key = 'c', scaleName = 'major', take = 36) => {
 
     const keyWithOctave = /[0-9]/.test(key) ? key : key + '3'
     const keyNextOctave = key.replace(/[0-9]/, found => Number(found) + 1)
@@ -18,6 +18,7 @@ module.exports = (key = 'c', scaleName = 'major') => {
         .concatMap(d => Rx.Observable.of(d).delay(150))
         .do(d => process.stdout.write(d))
         .skip(2)
+        .take(take)
         .map(digit => notes[Number(digit)])
         .map(instrument('koto'))
         .subscribe(play)
